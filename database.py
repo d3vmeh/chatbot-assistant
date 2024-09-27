01459f36b8917d5a2735d16c6373b010be8b16ce
d3vmeh <devxmehra@gmail.com>
2024-09-26 22:12:50 -0700

    Clearing all DBs

--- database.py
+++ database.py
@@ -5,17 +5,43 @@ from langchain.schema.document import Document
 from langchain_community.vectorstores.chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
 
-def load_transcription():
-    file = open("transcription.txt", "r")
-    transcription = file.read()
-    file.close()
-    document = [Document(transcription)]
-    return document
+from langchain.text_splitter import TokenTextSplitter
 
-def load_and_split():
-    document = load_transcription()
+import pdfplumber
+
+
+def load_doc(path):
+    doc_loader = PyPDFDirectoryLoader(path)
+    return doc_loader.load()
+
+
+def split_docs(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 60, length_function = len, is_separator_regex  = False)
-    return text_splitter.split_documents(document)
+    #text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=60)
+    #documents = text_splitter.split_documents(documents)
+    return text_splitter.split_documents(documents)
+
+def extract_pdf_text(uploaded_file):
+    total = ""
+    with pdfplumber.open(uploaded_file) as pdf:
+        for page in pdf.pages:
+            total += page.extract_text()
+        #pages = [page.extract_text() for page in pdf.pages]
+
+    return total
+
+#def add_to_chroma(chunks: list[Document]):
+    #database = Chroma(persist_directory=)
+def create_chunks(document,replace_newlines=False):#path, replace_newlines=False):
+    #document = load_doc(path)
+    chunks = split_docs(document)
+    if replace_newlines == True:
+        for i in range(len(chunks)):
+            chunks[i].page_content = chunks[i].page_content.replace("\n","")
+        return chunks
+    
+    return chunks
+
 
 def save_database(embeddings, chunks, path):    
     database = Chroma.from_documents(chunks,embeddings,persist_directory=path)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from database import *
 import shelve
+import os
+import shutil
 
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.llms.ollama import Ollama
@@ -8,6 +10,8 @@ from langchain_community.llms.ollama import Ollama
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+
+from langchain.schema.document import Document
 
 
 def load_chat_history():
@@ -54,26 +58,76 @@ def get_response(context, question, llm):
     
 
 embeddings = OpenAIEmbeddings()
+llm = Ollama(model="llama3.2",temperature=0.5)
 
 
+api_key = os.getenv("OPENAI_API_KEY")
+
+#chunks = load_and_split()
 #save_database(embeddings, chunks)
-db = load_database(embeddings)
-print("Ready to answer questions")
+#db = load_database(embeddings, "DBs/Chroma")
+#print("Ready to answer questions")
 
-st.title("Chat with Video Transcript")
+db =  None
+
+
+st.title("Chat with Files")
 
 if "messages" not in st.session_state:
     st.session_state.messages = load_chat_history()
 
-
+file_list = []
 with st.sidebar:
+
+
+    if "uploader_key" not in st.session_state:
+        st.session_state["uploader_key"] = 1
+
     if st.button("Clear Chat History"):
         st.session_state.messages = []
         save_chat_history([])
+    
+    if st.button("Clear Databases"):
+        for database_to_remove in os.listdir("DBs"):
+
+            shutil.rmtree(os.path.join("DBs", database_to_remove))
+            # p = os.path.join("DBs", database_to_remove)
+            # for file in os.listdir(p):
+            #     print(file, type(file))
+            #     os.remove(os.path.join(p, file))
+            # os.rmdir(os.path.join("DBs", database_to_remove))
+        print("===================\nDatabases cleared\n===================\n")
+        st.write("Databases cleared")
+        st.session_state["uploader_key"] += 1
+
+
+    uploaded_files = st.file_uploader(
+        "Choose a PDF file", 
+        accept_multiple_files=True,
+        key=st.session_state["uploader_key"]
+    )
+    if len(uploaded_files) > 0:
+        print(f"\n\n {len(uploaded_files)} Uploaded files")
+        print(uploaded_files)
+        print("\n\n\n")
+        for uploaded_file in uploaded_files:
+            if uploaded_file is not None:
+                file_list.append(uploaded_file.name)
+            bytes_data = uploaded_file.read()
+            st.write("filename:", uploaded_file.name)
+            #st.write(bytes_data)
+            filename = uploaded_file.name
+            db_path = os.path.join("DBs",filename)
+            file_text = extract_pdf_text(uploaded_file)
+            print(type(file_text))
+            doc = Document(file_text)
+            save_database(embeddings, create_chunks([doc]), db_path)
+
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
 
 if prompt := st.chat_input("How can I help?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
